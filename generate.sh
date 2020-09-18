6992ac71ed0e3af9a6ab165ec4cd9ab9c37e7821
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2020-09-19 02:49:55 +0700

    feat: add bash file to generate proto calculator

--- generate.sh
+++ generate.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Greet Service
 protoc --go_out=plugins=grpc:. greet/greetpb/greet.proto
+
+# Calculator Service
+protoc --go_out=plugins=grpc:. calculator/calculatorpb/calculator.proto
