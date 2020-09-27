dcee8afe14bc41f399e9fad1ad1bdf53d42bf16a
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2020-09-27 17:23:03 +0700

    feat: client streaming implementation

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/alibaihaqi/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
@@ -24,8 +25,8 @@ func main() {
 	// fmt.Println("Created client: %f", c)
 
 	// doUnaryCall(c)
-
-	doServerStreamingCall(c)
+	// doServerStreamingCall(c)
+	doClientStreamingCall(c)
 }
 
 func doUnaryCall(c greetpb.GreetServiceClient) {
@@ -69,4 +70,59 @@ func doServerStreamingCall(c greetpb.GreetServiceClient) {
 		}
 		log.Printf("Response from GreetManyTimes: %v", msg.GetResult())
 	}
+}
+
+func doClientStreamingCall(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Client Streaming RPC...")
+
+	requests := []*greetpb.LongGreetRequest{
+		&greetpb.LongGreetRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Jacky",
+				LastName:  "Hugo",
+			},
+		},
+		&greetpb.LongGreetRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+		},
+		&greetpb.LongGreetRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Lucy",
+				LastName:  "Chris",
+			},
+		},
+		&greetpb.LongGreetRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Mark",
+				LastName:  "Joe",
+			},
+		},
+		&greetpb.LongGreetRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Piper",
+				LastName:  "Hugo",
+			},
+		},
+	}
+
+	stream, err := c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling LongGreet: %v", err)
+	}
+
+	// we iterate over our slice and send each message individually
+	for _, req := range requests {
+		fmt.Printf("Sending request: %v\n", req)
+		stream.Send(req)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error while receiving response from LongGreet: %v", err)
+	}
+	fmt.Printf("LongGreet response: %v", res)
 }
