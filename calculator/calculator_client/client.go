35a9bae4b34fe4ad0ebd55133c4298d8cdf595a9
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2020-09-27 16:46:53 +0700

    feat: server streaming implementation exercise

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/alibaihaqi/grpc-go-course/calculator/calculatorpb"
@@ -21,7 +22,8 @@ func main() {
 
 	c := calculatorpb.NewCalculatorServiceClient(cc)
 
-	doUnaryCall(c) // RPC unary call
+	// doUnaryCall(c)           // RPC unary call
+	doServerStreamingCall(c) // RPC server streaming call
 }
 
 func doUnaryCall(c calculatorpb.CalculatorServiceClient) {
@@ -37,4 +39,28 @@ func doUnaryCall(c calculatorpb.CalculatorServiceClient) {
 		log.Fatalf("error while calling Sum RPC: %v", err)
 	}
 	log.Printf("Response from Sum: %v", res.SumNumber)
+}
+
+func doServerStreamingCall(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a Server Streaming RPC PrimeNumberDecomposition service")
+
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 120,
+	}
+
+	resStream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling PrimeNumberDecomposition RPC: %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// we've reached the end of the stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream: %v", err)
+		}
+		log.Printf("Response from PrimeNumberDecomposition: %v", msg.GetResultNumber())
+	}
 }
--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/alibaihaqi/grpc-go-course/calculator/calculatorpb"
 
@@ -26,6 +27,28 @@ func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculat
 	}
 
 	return res, nil
+}
+
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("PrimeNumberDecomposition function was invoked with %v\n", req)
+
+	num := req.GetNumber()
+	var pnum int32 = 2
+	for num > 1 {
+		if num%pnum == 0 {
+			fmt.Printf("current factor: %v\n", pnum)
+			res := &calculatorpb.PrimeNumberDecompositionResponse{
+				ResultNumber: fmt.Sprint(pnum),
+			}
+			num = num / pnum
+			stream.Send(res)
+			time.Sleep(1000 * time.Millisecond)
+		} else {
+			pnum++
+		}
+	}
+
+	return nil
 }
 
 func main() {
